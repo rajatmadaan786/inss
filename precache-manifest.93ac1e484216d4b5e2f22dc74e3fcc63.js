1aeb338bf5d68d9bd6139b4d88551bbdc86e3eae
rajatmadaan786 <rajatmadaan786.rm@gmail.com>
2020-07-12 01:46:07 +0530

    Updates

---
+++ precache-manifest.93ac1e484216d4b5e2f22dc74e3fcc63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d496bc1bad9e20faf6c9c4f64d55d434",
+    "url": "/inss/index.html"
+  },
+  {
+    "revision": "883286a06d9dc9616627",
+    "url": "/inss/static/css/main.d7ba6383.chunk.css"
+  },
+  {
+    "revision": "8823ad807fc9d8c01dca",
+    "url": "/inss/static/js/2.c0c6f46e.chunk.js"
+  },
+  {
+    "revision": "883286a06d9dc9616627",
+    "url": "/inss/static/js/main.688c85b3.chunk.js"
+  },
+  {
+    "revision": "f9d3c3fc4cedcdcbfecc",
+    "url": "/inss/static/js/runtime-main.b0f37b74.js"
+  }
+]);
